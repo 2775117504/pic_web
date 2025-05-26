b9177a8a64d895623dce71496a56da1e9a654b02
kwtx <2775117504@qq.com>
2025-05-27 01:08:10 +0800

    2

---
+++ src/test/java/com/example/picweb/PicWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.picweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
